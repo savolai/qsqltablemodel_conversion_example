7418e15bc5225b10247023ae832013c590baea92
Olli Savolainen <inbox@pilpi.net>
2015-07-10 17:24:48 +0300

    added dataChanged emission, still no bonus

--- tablemodel.cpp
+++ tablemodel.cpp
@@ -17,6 +17,7 @@ bool TableModel::setData(const QModelIndex &index, QVariant &value, int role){
     double baseValue=dValue*2;
 
     bool success=QSqlTableModel::setData(index,QVariant(baseValue),role);
+    emit dataChanged(index,index);
     return success;
 
 }
